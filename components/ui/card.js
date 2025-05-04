e7d05ceae0537aca84648541d8c56de0b8cf5210
GardenandCompany <till.schmidt-ott@ehl.ch>
2025-05-04 15:54:12 +0200

    Add files via upload

---
+++ components/ui/card.js
@@ -0,0 +1,12 @@
+
+export function Card({ children, ...props }) {
+  return (
+    <div {...props} className="rounded-2xl shadow bg-white hover:shadow-lg transition">
+      {children}
+    </div>
+  );
+}
+
+export function CardContent({ children, ...props }) {
+  return <div {...props}>{children}</div>;
+}
